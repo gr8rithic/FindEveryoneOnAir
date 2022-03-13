4104733c837b88e3f804f19be21a2cf0b0727e0b
gr8rithic <gr8rithic@gmail.com>
2022-03-13 11:43:51 +0530

    changes made

--- BackendCode/opensky.py
+++ BackendCode/opensky.py
@@ -1,8 +1,21 @@
 #for the swis area data
 
 import requests
+from geopy.geocoders import Nominatim
 
-url = "https://rithicsky:rithic2002@opensky-network.org/api/states/all?lamin=45.8389&lomin=5.9962&lamax=47.8229&lomax=10.5226"
+geolocator = Nominatim(user_agent="my_user_agent")
+city ="Kolkata"
+country ="India"
+loc = geolocator.geocode(city+','+ country)
+latitude = loc.latitude
+longitude = loc.longitude
+latitude_lower = latitude - 1
+latitude_higher = latitude + 1
+longitude_lower = longitude - 1
+longitude_higher = longitude + 1
+
+
+url = f"https://rithicsky:rithic2002@opensky-network.org/api/states/all?lamin={latitude_lower}&lomin={longitude_lower}&lamax={latitude_higher}&lomax={longitude_higher}"
 
 
 
